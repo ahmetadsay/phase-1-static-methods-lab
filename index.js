efcb5b4cf525d0c96a52ccbf266c9a726288010b
ahmet <adsayahmet94@hotmail.com>
2023-06-04 02:36:42 +0300

    assignmnet done

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9\- ']/g, '');
+  }
+
+  static titleize(string) {
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const words = string.split(' ');
+
+    const capitalizedWords = words.map((word, index) => {
+      if (index === 0 || !exceptions.includes(word)) {
+        return this.capitalize(word);
+      } else {
+        return word;
+      }
+    });
+
+    return capitalizedWords.join(' ');
+  }
 }
+
+// Testing the static methods
+console.log(Formatter.capitalize("hello")); // Output: Hello
+console.log(Formatter.sanitize("Hello, $world!")); // Output: Hello world
+console.log(Formatter.titleize("the great gatsby")); // Output: The Great Gatsby
+console.log(Formatter.titleize("i am the walrus")); // Output: I Am the Walrus
